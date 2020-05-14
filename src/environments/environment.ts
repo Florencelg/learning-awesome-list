b54aa7158e8cdb49ffd71ec35942efc3bc76c2e3
Florence Gautier <flo.lg.gautier@gmail.com>
2020-05-14 18:46:34 +0200

    mettre en place une architecture évolutive

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDkNFPkxRPGUex_gWsDmSCmqZNGVS7xV4M',
+    authDomain: 'awesome-list-45831.firebaseapp.com',
+    databaseURL: 'https://awesome-list-45831.firebaseio.com',
+    projectId: 'awesome-list-45831',
+    storageBucket: 'awesome-list-45831.appspot.com',
+    messagingSenderId: '275301632533',
+    appId: '1:275301632533:web:2767a04e3592069b2c9c1c',
+  },
 };
 
 /*
